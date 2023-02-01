e747fd2eff821917aae94b74f22f467825190176
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2023-02-01 15:58:50 -0800

    first commit

---
+++ string_sort.py
@@ -0,0 +1,23 @@
+#Name: Sophia Philips
+#GithubUsername: sophiacphilips
+#Date: 01/31/23
+#This code is designed to take a list of string and sort them alphabetically using the
+#insertion sort algorithm.
+
+
+
+def string_sort(string_list):
+  """
+  Sorts a list of strings in alphabetical order.
+  """
+  for index in range(1, len(string_list)):
+    letter = string_list[index]
+    pos = index - 1
+    while pos >= 0 and string_list[pos].lower > letter.lower: #".lower" changes strings to all lower case so they can be sorted alphabetically without case influence
+      string_list[pos + 1] = string_list[pos]
+      pos -= 1
+    string_list[pos + 1] = letter
+
+
+
+
