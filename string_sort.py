17ca8a25679d01f19f90393e5586f01f28457a02
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2023-02-01 16:00:46 -0800

    2nd commit

--- string_sort.py
+++ string_sort.py
@@ -13,7 +13,7 @@ def string_sort(string_list):
   for index in range(1, len(string_list)):
     letter = string_list[index]
     pos = index - 1
-    while pos >= 0 and string_list[pos].lower > letter.lower: #".lower" changes strings to all lower case so they can be sorted alphabetically without case influence
+    while pos >= 0 and string_list[pos].lower() > letter.lower(): #".lower" changes strings to all lower case so they can be sorted alphabetically without case influence
       string_list[pos + 1] = string_list[pos]
       pos -= 1
     string_list[pos + 1] = letter
